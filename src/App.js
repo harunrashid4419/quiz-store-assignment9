436ed8874945d39a9c907392629d744f2c583712
harunorrashid4419 <harunrashid4419@gmail.com>
2022-10-10 21:31:43 +0600

    header add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './components/Layout/Main';
+import Home from './components/Home/Home';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>,
+        },
+        {
+          path: '/home',
+          element: <Home></Home>
+        },
+      ]
+    }
+  ])
   return (
     <div className="App">
-      
+          <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="left">
+                <NavLink to='/home'>Select Quiz</NavLink>
+            </div>
+            <div className='menu'>
+                <nav>
+                    <ul>
+                        <li><NavLink to='/home'>Home</NavLink></li>
+                        <li><NavLink to='/quiz'>Quiz</NavLink></li>
+                        <li><NavLink to='/blog'>Blog</NavLink></li>
+                        <li><NavLink to='/chart'>Chart</NavLink></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
